f6dcaffe9c7533ce3f2a44055a050269f1945bc0
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-01 02:02:54 +0000

    Snapshot of branch master taken on 2019-04-30T22:02:50.909-04:00 from "https://github.com/UnofficialJuliaMirror/MIToS.jl-51bafb47-8a16-5ded-8b04-24ef4eede0b5"

--- test/PDB/Kabsch.jl
+++ test/PDB/Kabsch.jl
@@ -184,4 +184,23 @@
             @test rmsf(Vector{PDBResidue}[β1,β2]) == rmsf(Vector{PDBResidue}[β1,β2],[.5,.5])
         end
     end
+
+    @testset "PDBResidue without alpha-carbon" begin
+        small_2WEL = read(joinpath(pwd(), "data", "2WEL_D_region.pdb"), PDBFile)
+        small_6BAB = read(joinpath(pwd(), "data", "6BAB_D_region.pdb"), PDBFile)
+
+        aln_2WEL, aln_6BAB, RMSD = superimpose(small_2WEL, small_6BAB)
+        @test length(aln_2WEL) == 3
+        @test length(aln_6BAB) == 3
+        @test small_2WEL[2].atoms[1].coordinates ≉ aln_2WEL[2].atoms[1].coordinates
+        @test small_6BAB[2].atoms[1].coordinates ≉ aln_6BAB[2].atoms[1].coordinates
+        @test RMSD < 1.0e-14 # e.g. 6.9e-15, 3.7e-15 & 1.6e-15
+
+        @testset "PDB without CA" begin
+
+            filter!(atom -> atom.atom != "CA", small_2WEL[1].atoms)
+            filter!(atom -> atom.atom != "CA", small_6BAB[3].atoms)
+            @test_throws ArgumentError superimpose(small_2WEL, small_6BAB)
+        end
+    end
 end
